569a2599baf856199ba3bd3a3b0d0dae21483f29
RishabhSpark <rishabh1000khandelwal@gmail.com>
2025-05-05 11:41:16 +0000

    Day0

--- Day0/setup.py
+++ Day0/setup.py
@@ -2,12 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name="rishabh-hello",
-    version="1.1.4",
+    version="1.1.7",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     install_requires=[
-        "rich",
-        "typer[all]"
+        "typer>=0.6.0",
+        "rich>=13.0.0"
     ],
     entry_points={
         'console_scripts': [
